17604bc8700fe15ad831fc3ad84bbbc66989696c
elaineSommerville <elaine.sommerville@icloud.com>
2026-03-19 11:43:47 +0000

    simple POST endpoint created and tested

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import date
 
 app = FastAPI() #this is what was missing, i didnt define 'app'
 
+# initialize an empty applications array
+applications = [] 
+
+# message to ensure the server is working
 @app.get("/")
 def read_root():
 	return {"message": "Planning Tracker API running"}
+
+class Application(BaseModel):
+	id: int
+	address: str
+	description: str
+	status: str
+	submission_date: date
+
+# create a POST endpoint
+# this adds an application of type/model(BaseModel) 'Application' to applications array
+@app.post("/applications")
+def create_application(application: Application):
+	applications.append(application)
+	return application
