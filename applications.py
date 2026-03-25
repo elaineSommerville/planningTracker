f1ea983e4fc6b7e2b07b4d6fa636786b3eafc654
elaineSommerville <elaine.sommerville@icloud.com>
2026-03-25 20:51:57 +0000

    create the FastAPI route

---
+++ applications.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from services.application_service import get_applications
+
+router = APIRouter()
+
+@router.get("/applications")
+def read_applications(
+	status: str = None,
+	submission_date_from: str = None
+	submission_date_to: str = None
+	address: str = None, 
+	limit: int = 10,
+	offset: int = 0,
+	db: Session = Depends(get_db),
+):
+	return get_applications(
+		db,
+		status,
+		submission_date_from,
+		submission_date_to,
+		address,
+		limit,
+		offset,
+	)
