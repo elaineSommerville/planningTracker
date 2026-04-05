50ce5f503b9e1b41dde62c5f077580859efec3a7
elaineSommerville <elaine.sommerville@icloud.com>
2026-04-05 15:32:54 +0100

    fix database build

--- app/database.py
+++ app/database.py
@@ -1,15 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.orm import declarative_base
 
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/planning_db"
+DATABASE_URL = "postgresql://postgres:PostBoxGreen@localhost:5432/planning_db"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
+#this is what the route needs
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
 
 
 
